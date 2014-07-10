60c3e82953927fee6bde1f9b6de8ed66623d7d6d
zhumanyu <259571082@qq.com>
2014-07-10 14:51:04 +0800

    add

---
+++ VideoNews/Model/VNMessage.m
@@ -0,0 +1,43 @@
+//
+//  VNMessage.m
+//  VideoNews
+//
+//  Created by 曼瑜 朱 on 14-7-10.
+//  Copyright (c) 2014年 Manyu Zhu. All rights reserved.
+//
+
+#import "VNMessage.h"
+
+static NSString *kCid = @"pid";
+static NSString *kType = @"type";
+static NSString *kNews = @"news";
+static NSString *kSender = @"sender";
+
+
+@implementation VNMessage
+
+- (VNUser *)sender {
+    return makeSureNotNull([self.basicDict objectForKey:kSender]);
+}
+
+- (void)setSender:(VNUser *)sender{
+    [self.basicDict setObject:sender forKey:kSender];
+}
+
+- (VNUser *)news {
+    return makeSureNotNull([self.basicDict objectForKey:kNews]);
+}
+
+- (void)setNews:(VNNews *)news{
+    [self.basicDict setObject:news forKey:kNews];
+}
+
+- (int)pid {
+    return [makeSureNotNull([self.basicDict objectForKey:kCid]) intValue];
+}
+
+- (NSString *)type {
+    return makeSureNotNull([self.basicDict objectForKey:kType]);
+}
+
+@end
