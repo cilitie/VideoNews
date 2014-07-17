68704abb4f973e2764aca1e5f0f5e895d4b5970e
alientest <alienetest123@163.com>
2014-07-17 21:29:33 +0800

    增加个人主页UI

---
+++ VideoNews/Profile/VNProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  VNProfileViewController.h
+//  VideoNews
+//
+//  Created by liuyi on 14-7-17.
+//  Copyright (c) 2014年 Manyu Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VNProfileViewController : UIViewController
+
+@end
