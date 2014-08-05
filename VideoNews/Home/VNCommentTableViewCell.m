0ae03d3b059e9335c272147fdf4363dceb9dbfe7
alientest <alienetest123@163.com>
2014-08-05 14:16:45 +0800

    fix bug

--- VideoNews/Home/VNCommentTableViewCell.m
+++ VideoNews/Home/VNCommentTableViewCell.m
@@ -10,18 +10,24 @@
 
 @implementation VNCommentTableViewCell
 
+- (void)awakeFromNib {
+    [self.replyBtn setHitTestEdgeInsets:UIEdgeInsetsMake(-15.0, -15.0, -15.0, -15.0)];
+}
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
 }
+
 - (IBAction)replyButtonClicked:(UIButton*)sender {
     if([self.delegate respondsToSelector:@selector(replyButtonClicked:)])
    {
        [self.delegate replyButtonClicked:sender];
    }
 }
+
 - (IBAction)thumbnailClicked:(UIButton *)sender {
     if([self.delegate respondsToSelector:@selector(thumbnailClicked:)])
     {
