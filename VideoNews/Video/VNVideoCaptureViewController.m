06376b8166fab3832afae26095a958c56b8b9233
cilitie <cilitie@live.cn>
2014-08-22 13:51:19 +0800

    调整视频拍摄完成后红条

--- VideoNews/Video/VNVideoCaptureViewController.m
+++ VideoNews/Video/VNVideoCaptureViewController.m
@@ -462,7 +462,7 @@ static NSString *videoFilePath;
                 [[NSNotificationCenter defaultCenter] removeObserver:self name:@"ClearVideoClipsNotification" object:nil];
                 
                 __weak typeof(self) weakSelf = self;
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                     [weakSelf dismissViewControllerAnimated:YES completion:nil];
                 });
             }
